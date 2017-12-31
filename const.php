98a181ac3a31b1b0f7d20e407f442c9e14b16a15
Wataru Kume <kumewata@gmail.com>
2018-01-01 00:10:59 +0900

    add note about chapter1 and 2

---
+++ const.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kumew
+ * Date: 2017/12/31
+ * Time: 22:16
+ */
+
+//define('BOOK', 'Perfect PHP');
+const PPHP = 'Perfect PHPer';
--- index.php
+++ index.php
@@ -5,3 +5,13 @@
  * Date: 2017/12/31
  * Time: 19:51
  */
+
+//require_once 'const.php';
+//
+//define('BOOK', 'Perfect PHP');
+//
+//$value = 'BOOK';
+////echo constant($value), PHP_EOL;
+//echo BOOK, PHP_EOL;
+
+var_dump(__FILE__);
