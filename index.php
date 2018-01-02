8e11e95003c939f50971614bfc96f5e34fcbd426
Wataru Kume <kumewata@gmail.com>
2018-01-02 18:10:53 +0900

    p125 static property

--- index.php
+++ index.php
@@ -9,25 +9,8 @@
 class Employee
 {
     public $name;
-    private $state = '働いている'; // privateなのでクラスの内側からしかアクセスできない
-
-    public function getState() // privateなプロパティへアクセスするメソッド
-    {
-        return $this->state;
-    }
-
-    public function setState($state) //private なプロパティを変更するメソッド
-    {
-        return $this->state = $state;
-    }
-
-    public function work()
-    {
-        echo '書類を整理しています', PHP_EOL;
-    }
+    public static $company = '技評技術社';
 }
 
-$yamada = new Employee();
-$yamada->name = '山田';
-$yamada->job = 'プログラマ';
-echo $yamada->name, 'さんは', $yamada->job, 'です', PHP_EOL;
+// 静的なプロパティはインスタンス化されていなくても使うことができる。
+echo '従業員はみんな', Employee::$company, 'に努めています', PHP_EOL;
