63ae5fb18cd1eeb020f1cd5f1f9a8f434724a8e8
Wataru Kume <kumewata@gmail.com>
2018-01-05 22:58:32 +0900

    p141-145 magic_method overload

--- index.php
+++ index.php
@@ -6,14 +6,85 @@
  * Time: 19:51
  */
 
-class Employee
+class SomeClass
 {
-    // マジックメソッドは必ずpublicで定義する
-    public function __toString()
+    private $values = array(); // privateな値を保存するコンテナ
+
+    // #1 privateなコンテナへのアクセサ（getter）
+    // __get()：アクセス不能な変数が呼び出されたときに実行
+    public function __get($name)
     {
-        return 'This class is: ' . __CLASS__;
+        echo "get: $name", PHP_EOL;
+        if (!isset($this->values[$name])) {
+            throw new OutOfBoundsException($name . " not found.");
+        }
+        return $this->values[$name];
+    }
+
+    // #2 privateなコンテナへのアクセサ（setter）
+    // __set()：アクセス不能な変数に代入されたときに実行
+    public function __set($name, $value)
+    {
+        echo "set: $name setted to $value", PHP_EOL;
+        $this->values[$name] = $value;
+    }
+
+    public function __isset($name)
+    {
+        echo "isset: $name", PHP_EOL;
+        return isset($this->values[$name]);
+    }
+
+    public function __unset($name)
+    {
+        echo "unset: $name", PHP_EOL;
+        unset($this->values[$name]);
+    }
+
+    public function __call($name, $args)
+    {
+        echo "call: $name", PHP_EOL;
+
+        // アンダースコアをつけ、メソッド名とする
+        $method_name = '_' . $name;
+        if (!is_callable(array($this, $method_name))) {
+            throw new BadMethodCallException($name . " method not found.");
+        }
+
+        return call_user_func_array(array($this, $method_name), $args);
+    }
+
+    public static function __callStatic($name, $args)
+    {
+        echo "callStatic: $name", PHP_EOL;
+
+        $method_name = '_' . $name;
+        if (!is_callable(array('self', $method_name))) {
+            throw new BadMethodCallException($name . "method not found.");
+        }
+        return call_user_func_array(array('self', $method_name), $args);
+    }
+
+    private function _bar($value)
+    {
+        echo "bar called with arg '$value'", PHP_EOL;
+    }
+
+    private function _staticBar($value)
+    {
+        echo "staticBar called with arg '$value'", PHP_EOL;
     }
 }
 
-$yamada = new Employee();
-echo $yamada;
+$obj = new SomeClass();
+$obj->foo = 10;
+
+var_dump($obj->foo);
+var_dump(isset($obj->foo));
+var_dump(empty($obj->foo));
+
+unset($obj->foo);
+var_dump(isset($obj->foo));
+
+$obj->bar('baz');
+SomeClass::staticBar('baz');
