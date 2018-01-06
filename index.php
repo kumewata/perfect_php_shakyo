9b671e190a3e0bdfb22709adc4a3a3bbb6b9ab15
Wataru Kume <kumewata@gmail.com>
2018-01-06 21:53:16 +0900

    p147 autoload

--- index.php
+++ index.php
@@ -6,27 +6,12 @@
  * Time: 19:51
  */
 
-class Foo
+function __autoload($name)
 {
-    public function helloGateway()
-    {
-//        self::hello();
-        static::hello(); // 遅延静的束縛
-    }
-
-    public static function hello()
-    {
-        echo __CLASS__, ' hello!', PHP_EOL;
+    $filename = $name . '.php';
+    if (is_readable($filename)) {
+        require $filename;
     }
 }
 
-class Bar extends Foo
-{
-    public static function hello()
-    {
-        echo __CLASS__, ' hello!', PHP_EOL;
-    }
-}
-
-$bar = new Bar();
-$bar->helloGateway(); // Foo hello! ->Bar hello!
+$obj = new Foo();
