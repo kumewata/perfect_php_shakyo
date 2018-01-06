960282830257164be2987b243e72122fc54829b0
Wataru Kume <kumewata@gmail.com>
2018-01-06 21:47:47 +0900

    p146 late static bindings

--- index.php
+++ index.php
@@ -6,99 +6,27 @@
  * Time: 19:51
  */
 
-class SomeClass
+class Foo
 {
-    private $values = array(); // privateな値を保存するコンテナ
-
-    // #1 privateなコンテナへのアクセサ（getter）
-    // __get()：アクセス不能な変数が呼び出されたときに実行
-    public function __get($name)
+    public function helloGateway()
     {
-        echo "get: $name", PHP_EOL;
-        if (!isset($this->values[$name])) {
-            throw new OutOfBoundsException($name . " not found.");
-        }
-        return $this->values[$name];
+//        self::hello();
+        static::hello(); // 遅延静的束縛
     }
 
-    // #2 privateなコンテナへのアクセサ（setter）
-    // __set()：アクセス不能な変数に代入されたときに実行
-    public function __set($name, $value)
+    public static function hello()
     {
-        echo "set: $name setted to $value", PHP_EOL;
-        $this->values[$name] = $value; // $values arrayに要素が入れられる
-    }
-
-    // isset()あるいはempty()をアクセス不能プロパティに対して実行したときに実行される
-    public function __isset($name)
-    {
-        echo "isset: $name", PHP_EOL;
-        return isset($this->values[$name]);
-    }
-
-    // unset()をアクセス不能プロパティに対して実行したときに実行される。
-    public function __unset($name)
-    {
-        echo "unset: $name", PHP_EOL;
-        unset($this->values[$name]);
-    }
-
-    // アクセス不能なメソッドを呼び出したときに実行
-    public function __call($name, $args)
-    {
-        echo "call: $name", PHP_EOL;
-
-        // アンダースコアをつけ、メソッド名とする
-        $method_name = '_' . $name;
-        if (!is_callable(array($this, $method_name))) {
-            throw new BadMethodCallException($name . " method not found.");
-        }
-
-        return call_user_func_array(array($this, $method_name), $args);
-    }
-
-    // アクセス不能なメソッドをstaticに呼び出す
-    public static function __callStatic($name, $args)
-    {
-        echo "callStatic: $name", PHP_EOL;
-
-        $method_name = '_' . $name;
-        if (!is_callable(array('self', $method_name))) {
-            throw new BadMethodCallException($name . "method not found.");
-        }
-        return call_user_func_array(array('self', $method_name), $args);
-    }
-
-
-    private function _bar($value)
-    {
-        echo "bar called with arg '$value'", PHP_EOL;
-    }
-
-    private function _staticBar($value)
-    {
-        echo "staticBar called with arg '$value'", PHP_EOL;
-    }
-
-    /**
-     * @return array
-     */
-    public function getValues()
-    {
-        return $this->values;
+        echo __CLASS__, ' hello!', PHP_EOL;
     }
 }
 
-$obj = new SomeClass();
-$obj->foo = 10;
-//var_dump($obj->getValues());
+class Bar extends Foo
+{
+    public static function hello()
+    {
+        echo __CLASS__, ' hello!', PHP_EOL;
+    }
+}
 
-var_dump($obj->foo);
-var_dump(isset($obj->foo));
-var_dump(empty($obj->foo));
-
-unset($obj->foo);
-var_dump(isset($obj->foo));
-
-$obj->bar('baz');
-SomeClass::staticBar('baz');
+$bar = new Bar();
+$bar->helloGateway(); // Foo hello! ->Bar hello!
