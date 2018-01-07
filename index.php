30987bcb20facc651ffd27428a338997523501ff
Wataru Kume <kumewata@gmail.com>
2018-01-07 11:41:17 +0900

    p159 extended exception

--- index.php
+++ index.php
@@ -6,19 +6,26 @@
  * Time: 19:51
  */
 
+class ZeroDivisionException extends Exception
+{
+
+}
+
 function div($v1, $v2)
 {
     if ($v2 === 0) {
-        throw new Exception("arg #2 is zero.");
+        throw new ZeroDivisionException("arg #2 is zero.");
     }
     return $v1/$v2;
 }
 
 try {
-    echo div(1, 2), PHP_EOL;
-    echo div(1, 0), PHP_EOL;
-    echo div(2, 1), PHP_EOL;
+    echo div(1, 2);
+    echo div(1, 0);
+    echo div(2, 1);
+} catch (ZeroDivisionException $e) {
+    echo 'ZeroDivisionException!', PHP_EOL;
+    echo $e->getMessage(), PHP_EOL;
 } catch (Exception $e) {
-    echo 'Exception!', PHP_EOL;
     echo $e->getMessage(), PHP_EOL;
 }
