1c327652fae3afe81b213e187c96db987626ea2b
Wataru Kume <kumewata@gmail.com>
2018-01-14 22:39:49 +0900

    Controller class

---
+++ application/core/Controller.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kumew
+ * Date: 2018/01/14
+ * Time: 22:31
+ */
+
+abstract class Controller
+{
+    protected $controller_name;
+    protected $action_name;
+    protected $application;
+    protected $request;
+    protected $response;
+    protected $session;
+    protected $db_manager;
+
+    public function __construct($application)
+    {
+        $this->controller_name = strtolower(substr(get_class($this), 0, -10));
+
+        $this->application = $application;
+        $this->request     = $application->getRequest();
+        $this->response    = $application->getResponse();
+        $this->session     = $application->getSession();
+        $this->db_manager  = $application->getDbManager();
+    }
+}
