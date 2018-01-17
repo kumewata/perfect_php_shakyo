e7f224d358e8f9c31874ca190aebe92d3984a7c3
Wataru Kume <kumewata@gmail.com>
2018-01-17 23:27:24 +0900

    Controller::render() method

--- application/core/Controller.php
+++ application/core/Controller.php
@@ -40,4 +40,23 @@ abstract class Controller
 
         return $content;
     }
+
+    abstract function render($variables = array(), $template = null, $layout = 'layout')
+    {
+        $defaults = array(
+            'request' => $this->request,
+            'base_url' => $this->request->getBaseUrl(),
+            'session' => $this->session,
+        );
+
+        $view = new View($this->application-getViewDir(), $defaults);
+
+        if (is_null($template)) {
+            $template = $this->action_name;
+        }
+
+        $path = $this->controller_name . '/' . $template;
+
+        return $view->render($path, $variables, $layout);
+    }
 }
